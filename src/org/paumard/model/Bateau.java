4299baf7addab1e94adba1bbe017ee6e36229d0b
jpaumard <Jose.Paumard@gmail.com>
2014-02-10 10:13:24 +0100

    Added class Bateau

---
+++ src/org/paumard/model/Bateau.java
@@ -0,0 +1,53 @@
+package org.paumard.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Bateau {
+
+	private String nom ;
+	
+	private int tonnage ;
+	
+	private List<Marin> equipage ;
+	
+	
+	public Bateau() {
+		super();
+	}
+
+	public Bateau(String nom, int tonnage) {
+		super();
+		this.nom = nom;
+		this.tonnage = tonnage;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getTonnage() {
+		return tonnage;
+	}
+
+	public void setTonnage(int tonnage) {
+		this.tonnage = tonnage;
+	}
+	
+	public void addMarin(Marin marin) {
+		this.equipage.add(marin) ;
+	}
+	
+	public void removeMarin(Marin marin) {
+		this.equipage.remove(marin) ;
+	}
+	
+	public Collection<Marin> getEquipage() {
+		return new ArrayList<>(this.equipage) ;
+	}
+}
